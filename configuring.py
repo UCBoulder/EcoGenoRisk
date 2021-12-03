62bc545e44116afddfc052dadb85b33565d26d77
Anna-Ulanova <89947745+Anna-Ulanova@users.noreply.github.com>
2021-12-03 16:02:21 -0700

    Add files via upload

--- configuring.py
+++ configuring.py
@@ -15,47 +15,45 @@ path_folder = '/home/anna/PycharmProjects/pythonProject'
 text_file = 'assembly_summary.txt'
 header = {'User-Agent': str(ua.chrome)}
 
-print(header)
-temp_genome_list = requests.get(url, headers=header)
-with open(text_file,'w') as genome_list_out:
-    genome_list_out.write(print(temp_genome_list))
-genome_list_out.close()
+if os.path.exists(text_file):
+     print("File exists")
+else:
+     temp_genome_list = requests.get(url, headers=header)
+     temp_genome_list = requests.get(url)
+     print(temp_genome_list.text)
+     with open(text_file, 'w') as genome_list_out:
+         genome_list_out.write(temp_genome_list.text)
+     genome_list_out.close()
+
 processes = []
-count =0
-total=0
+count = 0
+total = 0
 
-# if os.path.exists(text_file):
-#     print("Assembly summary already exists")
-# else:
-#     wget.download(url, path_folder)
-
-with open(text_file,'r') as archaea_summary:
+file_object = open(text_file, 'a')
+with open(text_file ,'r') as archaea_summary:
     for line in archaea_summary:
         line1 = archaea_summary.readline()
-        if "Complete Genome" in line1:
-            output=line1.split("\t")
+        print(line1)
+        if line1.find('Complete Genome') != -1:
+            output = line1.split("\t")
             link = output[19]
             print(link)
-            #match= re.search("https://",line1)
-            #print(match.group())
-            #processes.append(line1.split()[-2])
-            #print(processes[count])
             org_name = link
             species_name = re.search(r'(.*)/(.*)', org_name).group(2)
-            # url_new = link[count]+'/'+species_name+'_protein.faa.gz'
-            # print(url_new)
+            url_new = link+'/'+species_name+'_protein.faa.gz'
+            #print(line)
+            #print(url_new)
             # wget.download(url_new)
             # file_name = species_name+'_protein.faa.gz'
             # destination = os.path.abspath('protein_file')
             # source = os.path.abspath(file_name)
             # shutil.move(source, destination+'/'+file_name)
-            count += 1
-            total += 1
-        else:
-            total += 1
+            total+=1
+        count+=1
+
+
 print(count)
 print(total)
 archaea_summary.close()
 #os.remove('assembly_summary.txt')
 print("That's all folks")
-

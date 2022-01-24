9c314a21193a26565cd74e6022009d698ecfeaa2
Anna-Ulanova <89947745+Anna-Ulanova@users.noreply.github.com>
2022-01-24 15:29:20 -0700

    Add files via upload

--- configuring.py
+++ configuring.py
@@ -1,13 +1,17 @@
-from helper_functions import input_domain, checking_assembly_file, file_extraction, file_management, diamond_impl
+from genomic_data_download import input_domain, checking_assembly_file, file_extraction, file_management, diamond_impl
 
 path_folder = '/home/anna/PycharmProjects/pythonProject'
 text_file = 'assembly_summary.txt'
-input_domain()
-url = input_domain()[0]
-domain = input_domain()[1]
+domain = input_domain()
+url = domain[0]
+domain = domain[1]
 checking_assembly_file(text_file, url)
 destination= file_extraction(text_file, domain)
+print(destination)
+
 file_management(destination)
-# diamond_impl(destination)
+diamond_impl(destination)
+
+
 
 print("That's all folks")
---
+++ genomic_data_download.py
@@ -0,0 +1,109 @@
+import gzip
+import os
+import re
+import shutil
+import requests
+import wget
+import subprocess
+from subprocess import PIPE, Popen
+import time
+from fake_useragent import UserAgent
+
+
+def input_domain():
+    domain = input("Enter domain:")
+    lower_case = domain.lower()
+    ncbi_url=''
+    if lower_case == 'archaea':
+        ncbi_url = 'https://ftp.ncbi.nlm.nih.gov/genomes/refseq/archaea/assembly_summary.txt'
+
+    elif lower_case == 'bacteria':
+        ncbi_url = 'https://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/assembly_summary.txt'
+
+    elif lower_case == 'fungi':
+        ncbi_url = 'https://ftp.ncbi.nlm.nih.gov/genomes/refseq/fungi/assembly_summary.txt'
+
+    else:
+        print("Invalid Input")
+
+    return ncbi_url, lower_case
+
+
+# Downloading/checking for assembly file
+def checking_assembly_file(text, link):
+    ua = UserAgent()
+    header = {'User-Agent': str(ua.chrome)}
+    if os.path.exists(text):
+        return "File exists"
+    else:
+        temp_genome_list = requests.get(link, headers=header)
+        temp_genome_list = requests.get(link)
+        print(temp_genome_list.text)
+        with open(text, 'w') as genome_list_out:
+            genome_list_out.write(temp_genome_list.text)
+        genome_list_out.close()
+
+
+# looping through assembly summary file, finding the samples that have a complete genome, and downloading the FTP file
+def file_extraction(text, dom):
+    with open(text, 'r') as assembly_summary:
+        assembly_summary.readline()
+        for line in assembly_summary:
+            output = line.split("\t")
+            if re.match(output[11], "Complete Genome"):
+                link = output[19]
+                org_name = link
+                species_name = re.search(r'(.*)/(.*)', org_name).group(2)
+                url_new = link + '/' + species_name + '_protein.faa.gz'
+                # wget.download(url_new)
+                download_name = species_name + '_protein.faa.gz'
+                destination = os.path.abspath(dom+ '_protein_file')
+                source = os.path.abspath(download_name)
+                # shutil.move(source, destination)
+    return destination
+    assembly_summary.close()
+
+
+# Extracting all files and deleting the zip folder
+def file_management(dest):
+    dir_name = 'x'
+    extension = ".gz"
+    file_name = ''
+    os.chdir(dest)
+    for item in os.listdir(dest):  # loop through items in dir
+        if item.endswith(extension):  # check for ".gz" extension
+            gz_name = os.path.abspath(item)  # get full path of files
+            file_name = (os.path.basename(gz_name)).rsplit('.', 1)[0]  # get file name for file within
+            with gzip.open(gz_name, "rb") as f_in, open(file_name, "wb") as f_out:
+                shutil.copyfileobj(f_in, f_out)
+            os.remove(gz_name)  # delete zipped file
+
+
+# DIAMOND IMPLEMENTATION
+def diamond_impl(dest):
+    #Muniprot = open('/home/anna/PycharmProjects/pythonProject/uniprot.fasta')
+    # reference = uniprot.read()
+    if os.path.abspath('home/anna/PycharmProjects/pythonProject/DIAMOND_matches') == 'EMPTY':
+        directory = '/home/anna/PycharmProjects/pythonProject/DIAMOND_matches'
+        os.makedirs(directory)
+
+    if os.path.abspath('home/anna/PycharmProjects/pythonProject/DIAMOND_matches/reference.dmnd') == 'EMPTY':
+        makedb = ['diamond', 'makedb', '--in', '/home/anna/PycharmProjects/pythonProject/uniprot.fasta', '-d', 'reference']
+        #lib = Popen(makedb, stdout=PIPE, shell=True)
+        subprocess.run(makedb)
+        # (output, err)=lib.communicate()
+        # p_status=lib.wait()
+
+    for item in os.listdir(dest):
+        if item.endswith('.faa'):
+            #print(counter)
+            file_path = os.path.abspath(item)
+            file_name = (os.path.basename(file_path)).rsplit('.', 1)[0]
+            matches = file_name + "_matches"
+            # Update for max-target and for output - just make like the other
+            blastp = ['diamond', 'blastp', '-d', 'reference', '-q', file_path, '-o', matches, '--max', '-target','-seqs', '1', '--outftm', '6']
+            subprocess.run(blastp)
+            # search=Popen(blastp, stdin=PIPE,stdout=PIPE)
+            #search=subprocess.call(blastp)
+            #search.communicate()
+            shutil.move(os.path.abspath(matches), directory)

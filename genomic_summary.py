38a54daf88631e18cfa385790e50113080a2d15c
Anna-Ulanova <89947745+Anna-Ulanova@users.noreply.github.com>
2022-02-08 17:09:36 -0700

    Add files via upload

--- genomic_summary.py
+++ genomic_summary.py
@@ -8,57 +8,42 @@ from numpy import savetxt
 
 def genome_extractor(dest):
     os.chdir("/home/anna/PycharmProjects/pythonProject/DIAMOND_matches")
-    ec_open = np.loadtxt("EC_num.csv",dtype='str')
-    #ec_table = ec_open.readlines()
-    #print(len(ec_table))
-    col = 1
-    #row_num = len(ec_table)
+    ec_open = np.loadtxt("EC_num.csv", dtype='str')
     print(ec_open)
-    # dimensions = ec_num.shape
-    # (row_num, colm) = dimensions
     name1 = np.zeros(len(ec_open))
-
     big_matrix = ["Name of Genome"]
-    for ec_force in ec_open:
+    for ec_force in ec_open:    #creates a title matrix  (genome name vs. EC number)
         big_matrix.append(ec_force)
     print(big_matrix)
-
     print(name1)
-
     counter = 0
-    # f = open('genome_output' , 'w')
     for item in os.listdir(dest):
-        if counter < 2:
-            counter += 1
-            if item.endswith("_matches"):
-        #    print(item)
-        #    gn = open(item, 'r')
-        #    line = gn.readline()
-        #    print(line)
-        #    temp_ec_match = numpy.zeros(row_num)
+        #if counter < 2: #allows only two genomes to be processed
+        #   counter += 1
+        if item.endswith("_matches"):
+            genome = [item]
+            for ec in ec_open:
+                fin = open(item, 'r')
+                ec_stand_in = np.zeros(len(ec_open))
+                ec_now = 0
+                for line in fin:
+                    x = line.split('\t')
+                    #print(x[1])
+                    y = x[1].split("?")
+                    #print(y[1])
+                    z = y[1].split(";_")
+                    additional = len(z)
+                    #print(additional)
+                    #print(z)
 
-        #    for row in range(1, row_num):
-        #        ec_entry = ec_table[row]
-        #        #print(ec_entry.strip())
-        #        if re.search(ec_entry.strip(), line):
-        #            temp_ec_match[row] = 1
-        #            print("match")
-        #        else:
-        #            temp_ec_match[row] = 0
-        #    col += 1
-        #    print(temp_ec_match)
-        #name1 = numpy.append(name1, temp_ec_match, axis=0)
-        #print(line)
-                genome = [item]
-                for ec in ec_open:
-                    fin = open(item,'r')
-                    ec_stand_in = np.zeros(len(ec_open))
-                    ec_now = 0
-                    for line in fin:
-                        if re.search(ec,line) is not None:
+                    if re.fullmatch(ec, y[1]) is not None:  # currently looks if the numbers are present and not for a direct match
+                        ec_now = 1
+                    # Extend list by that EC
+                    for i in z:
+                        if re.fullmatch(ec, i) is not None:
                             ec_now = 1
-                        #Extend list by that EC
-                    genome.append(ec_now)
-                big_matrix = np.vstack([big_matrix, genome])
-        print(big_matrix)
-    np.savetxt('big_matrix.cvs',big_matrix, fmt='%s')
+
+                genome.append(ec_now)
+            big_matrix = np.vstack([big_matrix, genome])
+    print(big_matrix)
+    np.savetxt('big_matrix.cvs', big_matrix, fmt='%s')

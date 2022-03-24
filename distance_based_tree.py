da76bd0b4a94e23dfa9fa68b4b49ae018c9750c0
Anna-Ulanova <89947745+Anna-Ulanova@users.noreply.github.com>
2022-03-24 10:40:56 -0600

    Add files via upload

--- distance_based_tree.py
+++ distance_based_tree.py
@@ -1,56 +1,69 @@
-# This function completes a genome to genome comparitive analysis.
+# # This function completes a genome to genome comparative analysis.
+# import os
+# import re
+# import numpy as np
+#
+# directory = '/home/anna/PycharmProjects/pythonProject/DIAMOND_matches'
+# os.chdir(directory)
+# horizontal = []
+# vertical=['']
+# with open('archaea_big_matrix.csv', 'r') as header:
+#     genome = header.readline()
+#     for genome in header:
+#         genome_name = genome.split()[0]
+#         if genome_name.startswith("Name_of_Genome"):
+#             print(" ")
+#         else:
+#             horizontal.append(genome_name)
+#             vertical = np.vstack([vertical, genome_name])
+# vertical=np.vstack([vertical,''])
+# matrix_2 = open('archaea_big_matrix.csv', 'r')
+# matrix_2_compare = matrix_2.readlines()
+# matrix_s=open('archaea_big_matrix.csv','r')
+# matrix_summary = matrix_s.readlines()
+# valid=0
+# comparison = []
+#
+# for line in matrix_summary:
+#     print("Processing new genome>>>")
+#     split_space = line.split()
+#     if split_space[0].startswith("Name_of_Genome"):
+#         print()
+#         valid = 1
+#     else:
+#         for line2 in matrix_2_compare:
+#             compare_line = line2.split()
+#             diff_sum = 0
+#             counter = 1
+#             valid = 0
+#             for bit in compare_line:
+#
+#                 if re.search("GCF", bit) or re.search("\.",bit) or re.search("Name_of_Genome",bit):
+#                     valid = 1
+#                 else:
+#                     diff_sum = diff_sum + abs(int(bit) - int(split_space[counter]))
+#                     counter += 1
+#                     valid = 0
+#             if valid == 0:
+#                 comparison.append(diff_sum)     #after this line runs, the matrix doesn't start a new row and just keeps adding more numbers
+#     if valid == 0:
+#         horizontal = np.vstack([horizontal, comparison])
+#         comparison = []
+#
+# save_mat=np.hstack([vertical,horizontal])
+# np.savetxt("Genome_to_genome_difference_comparison", save_mat, fmt='%s')
+# header.close()
+
+# This function completes a genome to genome comparative analysis.
 import os
 import re
 import numpy as np
 
 directory = '/home/anna/PycharmProjects/pythonProject/DIAMOND_matches'
 os.chdir(directory)
-# opens DIAMOND output, completes difference/similarity
-# read_file = open('archaea_big_matrix.csv', 'r')
-# matrix_summary = read_file.readlines()
-# head = open('archaea_big_matrix.csv', 'r')
-# sim_matrix = []
-# dif_matrix = []
-# genome_sim = []
-# genome_diff = ['Genome']
-# vertical = [' ']
-# horizontal = [' ']
-# for genome in head:
-#     genome_name = genome.split()[0]
-#     if genome_name.startswith("Name_of_Genome"):
-#         print(" ")
-#     else:
-#         horizontal.append(genome_name)
-#         vertical = np.vstack([vertical, genome_name])
-# for line in matrix_summary:
-#     if re.search(line, matrix_summary[0]):
-#         print(" ")
-#     else:
-#         standard = line.split()
-#         length = len(standard)
-#         genome_vertical = standard[0]
-#         for item in matrix_summary:
-#             if re.search(item, matrix_summary[0]):
-#                 print(" ")
-#             else:
-#                 comparison = item.split()
-#                 sim_sum = 0
-#                 diff_sum = 0
-#                 counter = 0
-#                 similarity = 0
-#                 for cell_value in comparison:
-#                     if cell_value == '1' or cell_value == '0':
-#                         counter += 1
-#                         diff_sum = diff_sum + abs(int(cell_value) - int(standard[counter]))
-#                 dif_matrix.append(diff_sum)
-#         genome_diff = np.vstack([genome_diff, diff_sum])
-# print("Let's see the shit show")
-# #genome_diff = np.stack([horizontal, genome_diff])
-# #genome_diff = vertical.append(genome_diff)
-# np.savetxt("Genome_to_genome_comparison", genome_diff, fmt='%s')
-horizontal = ['']
-comparison = []
-vertical=['']
+horizontal1 = []
+vertical1 = ['']
+
 with open('archaea_big_matrix.csv', 'r') as header:
     genome = header.readline()
     for genome in header:
@@ -58,39 +71,55 @@ with open('archaea_big_matrix.csv', 'r') as header:
         if genome_name.startswith("Name_of_Genome"):
             print(" ")
         else:
-            horizontal.append(genome_name)
-            vertiical = np.vstack([vertical, genome_name])
-
+            horizontal1.append(genome_name)
+            vertical1 = np.vstack([vertical1, genome_name])
+vertical1 = np.vstack([vertical1, ''])
+header.close()
 matrix_2 = open('archaea_big_matrix.csv', 'r')
 matrix_2_compare = matrix_2.readlines()
-matrix_s=open('archaea_big_matrix.csv','r')
+matrix_s = open('archaea_big_matrix.csv','r')
 matrix_summary = matrix_s.readlines()
-valid=0
-for line in matrix_summary:
-    print("OOWEEEE")    # currently prints 408 of those statements
+valid = 0
+comparison = []
+matrix=['']
+
+for line in matrix_summary:     # iterates through the lines of main matrix
+    print("Processing new genome>>>")
     split_space = line.split()
     if split_space[0].startswith("Name_of_Genome"):
-        print()
-        valid=1
+        print('')
+        valid = 1
     else:
-        for line2 in matrix_2_compare:
+        for line2 in matrix_2_compare:      # iterates through the lines of matrix to compare
             compare_line = line2.split()
-            diff_sum = 0
+            similarity = 0
             counter = 1
-            valid = 0
+            sim_sum = 0
             for bit in compare_line:
-                if re.search("GCF", bit) or re.search(".",bit):
+                if re.search("GCF", bit) or re.search("\.", bit) or re.search("Name_of_Genome", bit):
                     valid = 1
                 else:
-                    diff_sum = diff_sum + abs(int(bit) - int(split_space[counter]))
+                    valid = 0
+                    if bit == '1' and split_space[counter] == '1':
+                        similarity = 2
+                    elif bit == '0' and split_space[counter] == '0':
+                        similarity = 0
+                    else:
+                        similarity = -1
+                    # add = int(bit) + int(split_space[counter])
+                    # if add == 1:
+                    #     add = add * -1
                     counter += 1
-                    print(diff_sum)
-            comparison.append(diff_sum)     #after this line runs, the matrix doesn't start a new row and just keeps adding more numbers
-    if valid==0:
-        horizontal = np.vstack([horizontal, comparison])
+                    sim_sum = similarity + sim_sum
+            if valid == 0:
+                comparison.append(sim_sum)  # after this line runs, the matrix doesn't start a new row and just keeps adding more numbers
+    if valid == 0:
+        horizontal1 = np.vstack([horizontal1, comparison])
+        comparison = []
 
+np.savetxt("Yeet",horizontal1, fmt='%s')
+if len(matrix) != 1:
+    matrix=np.vstack([horizontal1, matrix])
+    save_mat = np.hstack([vertical1, matrix])
+    np.savetxt("Genome_to_genome_similarity_comparison", save_mat, fmt='%s')
 
-np.savetxt("Genome_to_genome_comparison_check", horizontal, fmt='%s')
-#if os.path.abspath("Genome_to_genome_comparison_check"):
-    #yeet = np.vstack([vertical, horizontal])
-    #np.savetxt("Genome_to_genome_comparison_w/_headers", vertical, fmt='%s')
--- genomic_data_download.py
+++ genomic_data_download.py
@@ -13,57 +13,70 @@ from fake_useragent import UserAgent
 # Asks for input for domain, returns a specific url
 def input_domain():
     domain = input("Enter domain:")
+    path = input("Enter desired pathway to make a new folder in: ")
+    # os.chdir(path)
+    # os.makedirs(domain)
     lower_case = domain.lower()
     ncbi_url = ''
     if lower_case == 'archaea':
         ncbi_url = 'https://ftp.ncbi.nlm.nih.gov/genomes/refseq/archaea/assembly_summary.txt'
-
     elif lower_case == 'bacteria':
         ncbi_url = 'https://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/assembly_summary.txt'
-
     elif lower_case == 'fungi':
         ncbi_url = 'https://ftp.ncbi.nlm.nih.gov/genomes/refseq/fungi/assembly_summary.txt'
-
     else:
         print("Invalid Input")
-
-    return ncbi_url, lower_case  # returns domain name and url
-
+    pathway = path+'/'+domain
+    os.chdir(pathway)
+    print(pathway)
+    return [ncbi_url, lower_case] # returns domain name and url
 
 # Downloading/checking for assembly file
 def checking_assembly_file(text, link):  # asks for assembly_summary text file name, requires download link
+    print(os.getcwd())
     ua = UserAgent()
     header = {'User-Agent': str(ua.chrome)}
     if os.path.exists(text):  # checks if file exists already in order to not spam NCBI
+        time.sleep(4)
         return "File exists"
     else:
+        time.sleep(4)
         temp_genome_list = requests.get(link, headers=header)  # permissions for opening file through NCBI
         # temp_genome_list = requests.get(link)
         print(temp_genome_list.text)
+
         with open(text, 'w') as genome_list_out:
             genome_list_out.write(
                 temp_genome_list.text)  # copies content from the website, pastes it into assembly_summary file
         genome_list_out.close()
+        print("checking_assembly_file--success")
 
 
 # looping through assembly summary file, finding the samples that have a complete genome, and downloading the FTP file
 def file_extraction(text, dom):
-    with open(text, 'r') as assembly_summary:
-        assembly_summary.readline()  # reads line by line, iterating through the doc
-        for line in assembly_summary:
-            output = line.split("\t")  # separating line by tab
-            if re.match(output[11], "Complete Genome"):
-                link = output[19]  # finds ftp path
-                org_name = link
-                species_name = re.search(r'(.*)/(.*)', org_name).group(2)
-                url_new = link + '/' + species_name + '_protein.faa.gz'  # finds exact address to download the exact url of the FTP file
-                # wget.download(url_new)
-                download_name = species_name + '_protein.faa.gz'
-                destination = os.path.abspath(dom + '_protein_file')  # creates a domain specified folder pathway
-                source = os.path.abspath(download_name)
-                # shutil.move(source, destination)   # moves file into domain specified folder
+    print(os.getcwd())
+    if os.path.exists(os.getcwd()+"/"+dom+"_protein_file"):
+        print('')
+        destination = (os.getcwd()+"/"+dom+"_protein_file")
+    else:
+        os.makedirs(dom+"_protein_file")
+        destination = os.path.abspath(dom + '_protein_file')  # creates a domain specified folder pathway
+        with open(text, 'r') as assembly_summary:
+            assembly_summary.readline()  # reads line by line, iterating through the doc
+            for line in assembly_summary:
+                output = line.split("\t")  # separating line by tab
+                if re.match(output[11], "Complete Genome"):
+                    link = output[19]  # finds ftp path
+                    org_name = link
+                    species_name = re.search(r'(.*)/(.*)', org_name).group(2)
+                    url_new = link + '/' + species_name + '_protein.faa.gz'  # finds exact address to download the exact url of the FTP file
+                    #wget.download(url_new)
+                    download_name = species_name + '_protein.faa.gz'
+                    source = os.path.abspath(download_name)
+                    #shutil.move(source, destination)   # moves file into domain specified folder
+        assembly_summary.close()
+        print("file_extraction--success")
     return destination
-    assembly_summary.close()
 
 
 # Extracting all files and deleting the zip folder
@@ -80,21 +93,24 @@ def file_management(dest):  # takes the destination of the domain folder
                 shutil.copyfileobj(f_in, f_out)
             os.remove(gz_name)  # delete zipped file
 
+    print("file_management-- success")
+
 
 # DIAMOND IMPLEMENTATION
 def diamond_impl(dest):  # takes domain folder path
 
-    directory='/home/anna/PycharmProjects/pythonProject/DIAMOND_matches'
-    if os.path.exists(directory):
-        print(directory)
-        os.chdir(directory)
-    else:
-        os.chdir('/home/anna/PycharmProjects/pythonProject')
-        os.makedirs('DIAMOND_matches')  # if library is present, the directory is changed to the folders path
-        os.chdir(directory)
-        print(directory)
+    current_dir=os.getcwd()
+    directory=current_dir.rsplit('/',1)[0]
+    print(directory)
+    os.chdir(directory)
+    # if os.path.exists(directory):
+    #     print(directory)
+    #     os.chdir(directory)
+    # else:
+    os.makedirs('DIAMOND_matches')  # if library is present, the directory is changed to the folders path
+    print(directory)
 
-    if os.path.exists('/home/anna/PycharmProjects/pythonProject/DIAMOND_matches/reference.dmnd'):  # if there currently is no reference library (.dmnd), makedb creates a DIAMOND library
+    if os.path.exists(directory+'/reference.dmnd'):  # if there currently is no reference library (.dmnd), makedb creates a DIAMOND library
         print("Library Detected")
     else:
         makedb = ['diamond', 'makedb', '--in', '/home/anna/PycharmProjects/pythonProject/uniprot.fasta', '-d',
@@ -106,13 +122,14 @@ def diamond_impl(dest):  # takes domain folder path
     for item in os.listdir(dest):  # for item in domain_folder, if item is a faa file, complete diamond analysis
         if item.endswith('.faa'):
             file_path = os.path.abspath(item)
-            print(file_path)
+            #print(file_path)
             file_name = (os.path.basename(file_path)).rsplit('.', 1)[0]  # name of sample
             matches = file_name + "_matches"
             if os.path.exists(directory+"/"+matches):
                 print(" ")
             else:
-                blastp = ['diamond', 'blastp', '-d', '/home/anna/PycharmProjects/pythonProject/DIAMOND_matches/reference.dmnd', '-q', file_path, '-o', matches, '--max-target-seqs', '1', '--outfmt', '6']  # completes DIAMOND search by using full path
+                blastp = ['diamond', 'blastp', '-d', directory+'/reference.dmnd', '-q', file_path, '-o', matches, '--max-target-seqs', '1', '--outfmt', '6']  # completes DIAMOND search by using full path
                 subprocess.run(blastp)
                 shutil.move(os.path.abspath(matches), directory)  # moves to DIAMOND folder
+    print("diamond_impl--success")
     return directory

101a78edd63b5ceb304e79d136b14574ddffe2bd
Anna-Ulanova <89947745+Anna-Ulanova@users.noreply.github.com>
2022-05-05 14:51:24 -0600

    Add files via upload

--- genomic_data_download.py
+++ genomic_data_download.py
@@ -26,10 +26,11 @@ def input_domain():
         ncbi_url = 'https://ftp.ncbi.nlm.nih.gov/genomes/refseq/fungi/assembly_summary.txt'
     else:
         print("Invalid Input")
-    pathway = path+'/'+domain
+    pathway = path + '/' + domain
     os.chdir(pathway)
     print(pathway)
-    return [ncbi_url, lower_case] # returns domain name and url
+    return [ncbi_url, lower_case]  # returns domain name and url
+
 
 # Downloading/checking for assembly file
 def checking_assembly_file(text, link):  # asks for assembly_summary text file name, requires download link
@@ -55,11 +56,11 @@ def checking_assembly_file(text, link):  # asks for assembly_summary text file n
 # looping through assembly summary file, finding the samples that have a complete genome, and downloading the FTP file
 def file_extraction(text, dom):
     print(os.getcwd())
-    if os.path.exists(os.getcwd()+"/"+dom+"_protein_file"):
+    if os.path.exists(os.getcwd() + "/" + dom + "_protein_file"):
         print('')
-        destination = (os.getcwd()+"/"+dom+"_protein_file")
+        destination = (os.getcwd() + "/" + dom + "_protein_file")
     else:
-        os.makedirs(dom+"_protein_file")
+        os.makedirs(dom + "_protein_file")
         destination = os.path.abspath(dom + '_protein_file')  # creates a domain specified folder pathway
         with open(text, 'r') as assembly_summary:
             assembly_summary.readline()  # reads line by line, iterating through the doc
@@ -70,14 +71,16 @@ def file_extraction(text, dom):
                     org_name = link
                     species_name = re.search(r'(.*)/(.*)', org_name).group(2)
                     url_new = link + '/' + species_name + '_protein.faa.gz'  # finds exact address to download the exact url of the FTP file
-                    #wget.download(url_new)
-                    download_name = species_name + '_protein.faa.gz'
-                    source = os.path.abspath(download_name)
-                    #shutil.move(source, destination)   # moves file into domain specified folder
+                    if os.path.exists(os.getcwd() + "/" + dom + "_protein_file" + "/" + species_name + "_protein.faa"):
+                        print("")
+                    else:
+                        wget.download(url_new)
+                        download_name = species_name + '_protein.faa.gz'
+                        source = os.path.abspath(download_name)
+                        shutil.move(source, destination)   # moves file into domain specified folder
         assembly_summary.close()
         print("file_extraction--success")
     return destination
-
 
 # Extracting all files and deleting the zip folder
 def file_management(dest):  # takes the destination of the domain folder
@@ -99,37 +102,40 @@ def file_management(dest):  # takes the destination of the domain folder
 # DIAMOND IMPLEMENTATION
 def diamond_impl(dest):  # takes domain folder path
 
-    current_dir=os.getcwd()
-    directory=current_dir.rsplit('/',1)[0]
+    current_dir = os.getcwd()
+    directory = current_dir.rsplit('/', 1)[0]
     print(directory)
     os.chdir(directory)
-    # if os.path.exists(directory):
-    #     print(directory)
-    #     os.chdir(directory)
-    # else:
-    os.makedirs('DIAMOND_matches')  # if library is present, the directory is changed to the folders path
-    print(directory)
+    new_destination = directory + '/DIAMOND_matches'
+    print(new_destination)
+    if os.path.exists(new_destination):
+        print(" ")
+        os.chdir(directory+'/DIAMOND_matches')
+    else:
+        os.makedirs('DIAMOND_matches')  # if library is present, the directory is changed to the folders path
 
-    if os.path.exists(directory+'/reference.dmnd'):  # if there currently is no reference library (.dmnd), makedb creates a DIAMOND library
+    if os.path.exists(
+            directory + '/reference.dmnd'):  # if there currently is no reference library (.dmnd), makedb creates a DIAMOND library
         print("Library Detected")
     else:
         makedb = ['diamond', 'makedb', '--in', '/home/anna/PycharmProjects/pythonProject/uniprot.fasta', '-d',
                   'reference']  # reference library full pathway
         subprocess.run(makedb)
         print("Library complete")
-
     os.chdir(dest)
     for item in os.listdir(dest):  # for item in domain_folder, if item is a faa file, complete diamond analysis
         if item.endswith('.faa'):
             file_path = os.path.abspath(item)
-            #print(file_path)
+            # print(file_path)
             file_name = (os.path.basename(file_path)).rsplit('.', 1)[0]  # name of sample
             matches = file_name + "_matches"
-            if os.path.exists(directory+"/"+matches):
+            print(matches)
+            if os.path.exists(new_destination+"/"+matches):
                 print(" ")
             else:
-                blastp = ['diamond', 'blastp', '-d', directory+'/reference.dmnd', '-q', file_path, '-o', matches, '--max-target-seqs', '1', '--outfmt', '6']  # completes DIAMOND search by using full path
+                blastp = ['diamond', 'blastp', '-d', directory + '/reference.dmnd', '-q', file_path, '-o', matches,
+                          '--max-target-seqs', '1', '--outfmt', '6']  # completes DIAMOND search by using full path
                 subprocess.run(blastp)
-                shutil.move(os.path.abspath(matches), directory)  # moves to DIAMOND folder
+                shutil.move(os.path.abspath(matches), new_destination)  # moves to DIAMOND folder
     print("diamond_impl--success")
     return directory

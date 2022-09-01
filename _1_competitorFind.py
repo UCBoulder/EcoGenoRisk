7aaae699cf13b2eaf1b3b3944cd1d41154f5814b
Anna-Ulanova <89947745+Anna-Ulanova@users.noreply.github.com>
2022-09-01 16:21:47 -0600

    Add files via upload

--- _1_competitorFind.py
+++ _1_competitorFind.py
@@ -18,8 +18,9 @@ def ec_locator(sb_name, pathway):
     top_match_row = find_top_match.head(1).reset_index(drop=True)
     # Extracts the top match genome name
     top_match_name = top_match_row.iloc[0, 0]
+    print('Top Match Found is: ', top_match_name)
     # Reads document which has EC BSM for Bacteria, Archaea, and the synbio organism
-    ec_bsm = pd.read_csv(pathway + '/complete_binary_matrix_5_new_zeros.txt', delimiter='\t', header=0, index_col=0)
+    ec_bsm = pd.read_csv(pathway+'/complete_binary_matrix _CompFind.txt', delimiter='\t', header=0, index_col=0)
     # Finds the EC BSM for top match organism and transforms it into a data frame
     top_match_bsm = pd.DataFrame(ec_bsm.loc[top_match_name], columns=[top_match_name])
     # Sets index name of dataframe to EC Number
@@ -34,7 +35,7 @@ def ec_locator(sb_name, pathway):
     # Finds occurances where the synbio and top match EC BSMs don't have the same binary values and extracts EC numbers
     different_ECs = different_ECs[different_ECs[sb_name] != different_ECs[top_match_name]]
     # Turn on to save the list of EC numbers that are different between the two genomes and where the EC number is present
-    different_ECs.to_csv('different_EC_profile*.txt', header=True, index=True, sep='\t')
+    different_ECs.to_csv('different_EC_profile.txt', header=True, index=True, sep='\t')
     print('Locating Variable EC numbers Complete')
     return different_ECs, top_match_bsm, synbio_bsm
 
@@ -46,7 +47,7 @@ def ec_locator(sb_name, pathway):
 # Output for this function is dataframe which contain InChIKeys of substrates that are shared between the synbio
 # organism and the top match
 # Method is a "catch all", no matter what kind of changes occur in the EC BSM
-def substrate_changes_synbio_v_chassis(pathway, chassis_bsm, synbio_bsm):
+def substrate_changes_synbio_v_chassis(pathway, chassis_bsm, synbio_bsm, different_ECs):
     # Opens MetaCyc list of all reactions
     metacyc_all_rxns = pd.read_csv(pathway + '/All-reactions-of-MetaCyc.txt',
                                    delimiter='\t', header=0, index_col=0)
@@ -57,13 +58,13 @@ def substrate_changes_synbio_v_chassis(pathway, chassis_bsm, synbio_bsm):
     # Merges based on EC number to create a list of reactions/substrates occurring in top match
     top_match_rxns = pd.merge(chassis_bsm, metacyc_all_rxns, on='EC-Number', how='inner')
     # Turn on to save the list of substrates found in top match
-    top_match_rxns.to_csv('chassis_all_rxns*.txt', header=True, index= True, sep='\t')
+    #top_match_rxns.to_csv('chassis_all_rxns.txt', header=True, index= True, sep='\t')
     # Isolates the InChI Key column and splits the column based on // to isolate all substrates for top match
     chassis_InChI_Key = pd.DataFrame(top_match_rxns['InChI-Key'].str.split('//', expand=True))
     # Merges based on EC number to create a list of reactions/substrates occurring in synbio
     synbio_rxns = pd.merge(synbio_bsm, metacyc_all_rxns, on='EC-Number', how='inner')
     # Turn on to save the list of substrates found in synbio
-    synbio_rxns.to_csv('synbio_all_rxns*.txt', header=True, index= True, sep='\t')
+    #synbio_rxns.to_csv('synbio_all_rxns.txt', header=True, index= True, sep='\t')
     # Isolates the InChI Key column and splits the column based on // to isolate all substrates for synbio
     synbio_InChI_Key = pd.DataFrame(synbio_rxns['InChI-Key'].str.split('//', expand=True))
     # Creates an array of InChI Keys of substrates that can be found in both organisms, saves the array as index
@@ -77,8 +78,7 @@ def substrate_changes_synbio_v_chassis(pathway, chassis_bsm, synbio_bsm):
         chassis_vs_synbio += chassis_synbio_single_col
     chassis_one_column_df = pd.DataFrame(chassis_vs_synbio, columns=['InChI_Key'])
     # Removes any spaces
-    chassis_one_column_df = chassis_one_column_df[
-        chassis_one_column_df['InChI_Key'].str.contains(' ') == False]
+    chassis_one_column_df['InChI_Key'] = chassis_one_column_df['InChI_Key'].str.strip()
     # Finds unique InChI Keys in the list
     unique_chassis_InChI_Key = chassis_one_column_df.drop_duplicates()
     # Excludes InChIKey for water to give other substrates a higher priority
@@ -88,7 +88,7 @@ def substrate_changes_synbio_v_chassis(pathway, chassis_bsm, synbio_bsm):
     unique_chassis_InChI_Key = unique_chassis_InChI_Key[
         unique_chassis_InChI_Key['InChI_Key'].str.contains('None') == False]
     # Saves list of InChI Keys
-    numpy.savetxt('synbiovschassis_inchikey*.txt', unique_chassis_InChI_Key, fmt='%s', delimiter='\t')
+    numpy.savetxt('synbiovschassis_inchikey.txt', unique_chassis_InChI_Key, fmt='%s', delimiter='\t')
     print('Top Match vs. Synbio InChI Key Substrates Analysis Is Complete')
     return unique_chassis_InChI_Key
 
@@ -112,7 +112,7 @@ def substrate_changes_modified_pathway(pathway, different_ECs):
     # Finds the reactions associated with the modified pathway
     merged_rxns = pd.merge(different_ECs, metacyc_all_rxns, on='EC-Number', how='inner')
     # Turn on to save the list of all reactions that occur due to modified pathways
-    merged_rxns.to_csv('altered_pathway_merged_rxns*.txt', header=True, index = True, sep='\t')
+    merged_rxns.to_csv('altered_pathway_merged_rxns.txt', header=True, index = True, sep='\t')
     # Splits dataframes based on whether the top match EC numbers are being turned on
     top_match_modification = merged_rxns[merged_rxns.iloc[:, 1] == 1]
     # Isolates the InChI Key column and splits the column based on // to isolate all substrates for top match
@@ -131,30 +131,39 @@ def substrate_changes_modified_pathway(pathway, different_ECs):
     mod_pathway = np.isin(top_match_mod_InChIKey, synbio_mod_InChIKey)
     # Returns the InChI Key names by referencing the index
     pathway_shared_InChI_Key = top_match_mod_InChIKey[mod_pathway]
+    np.savetxt('raw_unprocessed_shared_keys.txt', pathway_shared_InChI_Key, fmt = '%s', delimiter='\t')
     # Converts array into a single column list
     pathway_comp = []
     for column in pathway_shared_InChI_Key:
         pathway_list = pathway_shared_InChI_Key[column].tolist()
         pathway_comp += pathway_list
     modified_pathway_column = pd.DataFrame(pathway_comp, columns=['InChI_Key'])
+    print(modified_pathway_column)
+    print(type(modified_pathway_column))
+    # Removes white space from the column
+    modified_pathway_column['InChI_Key'] = modified_pathway_column['InChI_Key'].str.strip()
     # Finds unique InChI Keys in the list
-    unique_path_InChI_Key = modified_pathway_column['InChI_Key'].unique()
+    unique_path_InChI_Key = modified_pathway_column.drop_duplicates()
     # Excludes InChIKey for water to give other substrates a higher priority
     unique_path_InChI_Key = unique_path_InChI_Key[
-        unique_path_InChI_Key != 'InChIKey=XLYOFNOQVPJJNP-UHFFFAOYSA-N']
+        unique_path_InChI_Key['InChI_Key'].str.contains('InChIKey=XLYOFNOQVPJJNP-UHFFFAOYSA-N') == False]
     # Removes any InChI Keys that were listed as None
-    unique_path_InChI_Key = unique_path_InChI_Key[unique_path_InChI_Key != 'None']
+    unique_path_InChI_Key = unique_path_InChI_Key[
+        unique_path_InChI_Key['InChI_Key'].str.contains('None') == False]
     # Saves list of InChI Keys
-    numpy.savetxt('modifiedpathway_inchikey*.txt', unique_path_InChI_Key, fmt='%s', delimiter='\t')
+    numpy.savetxt('modifiedpathway_inchikey.txt', unique_path_InChI_Key, fmt='%s', delimiter='\t')
     print('Modified Pathway Substrates Analysis Is Complete')
     return unique_path_InChI_Key
 ##====================================================================================================================##
 
-
-synbio = 'E_Coli_Chimeria'
+#synbio = 'Aquificota_Actinobacteria_Chimera'
+#synbio = 'Aquificota_Actinobacteria_Chimera_935_Off'
+#path = '/home/anna/Desktop/EcoGenoRisk/HazID/NicheOverlap/Different'
 path = '/home/anna/Desktop/EcoGenoRisk/HazID/NicheOverlap/Similar'
+#synbio = 'E_Coli_Chimera'
+synbio = 'E_Coli_Chimera_1172_Off'
 [different_ECs, chassis_bsm, synbio_bsm] = ec_locator(synbio, path)
 pathway = '/home/anna/Desktop/EcoGenoRisk/HazID/CompetitorFind'
-individual_genome_rxns = substrate_changes_synbio_v_chassis(pathway, synbio_bsm, chassis_bsm)
+individual_genome_rxns = substrate_changes_synbio_v_chassis(pathway, synbio_bsm, chassis_bsm, different_ECs)
 pathway_modifcation = substrate_changes_modified_pathway(pathway, different_ECs)
 print(individual_genome_rxns, '\n', pathway_modifcation)

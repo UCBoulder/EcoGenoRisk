aec239e55f1aafedfe04a3a1e599549bcd9b7ba8
jtdocter27 <john.docter@colorado.edu>
2024-08-30 09:23:06 -0600

    test

--- _1_competitorFind.py
+++ _1_competitorFind.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import os as os
 from scoring import scoring, finding_distance
-
+#nonsense add
 
 ##====================================================================================================================##
 

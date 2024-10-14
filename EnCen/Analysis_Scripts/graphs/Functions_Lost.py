1fc41b8597f44be28d272dd4c5069b78acb14429
jtdocter27 <john.docter@colorado.edu>
2024-10-14 14:57:33 -0600

    Updated font sizes

--- EnCen/Analysis_Scripts/graphs/Functions_Lost.py
+++ EnCen/Analysis_Scripts/graphs/Functions_Lost.py
@@ -7,8 +7,8 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-activated_sludge= pd.read_excel('/home/anna/Desktop/JD_Niche_OverLap (Git)/Niche_JD/Eco_V2/EnCen/Paper Results/Biome Analysis Results/Activated Sludge/Lost_Functions.xlsx')
-activated_sludge_all = pd.read_excel('/home/anna/Desktop/JD_Niche_OverLap (Git)/Niche_JD/Eco_V2/EnCen/Paper Results/Biome Analysis Results/Activated Sludge/Biome Synbio Top Match EC Comparison.xlsx')
+activated_sludge= pd.read_excel('/home/anna/Desktop/JD_Niche_OverLap (Git)/Niche_JD/Eco_V2/EnCen/Paper Results/Biome Analysis Results/Cow Rumen/Lost_Functions.xlsx')
+activated_sludge_all = pd.read_excel('/home/anna/Desktop/JD_Niche_OverLap (Git)/Niche_JD/Eco_V2/EnCen/Paper Results/Biome Analysis Results/Cow Rumen/Biome Synbio Top Match EC Comparison.xlsx')
  #remember lost functions are just those that the topmatch bin has and the invading organism does not have. 
 # print(activated_sludge.head(10))
 
@@ -27,14 +27,16 @@ activated_sludge2 = activated_sludge[['EC Class and Subclass', 'Percentage', 'Na
 
 
 ##This is the Bivariate Plot__________________________________________________________________________________________________
-# sns.scatterplot(data=activated_sludge2, x= 'EC Class and Subclass', y='Percentage', color = 'blue', hue='Name',s=200) #Bivariate Plot 
+# plot = sns.scatterplot(data=activated_sludge2, x= 'EC Class and Subclass', y='Percentage', color = 'blue', hue='Name',s=200) #Bivariate Plot 
 # plt.tight_layout(rect=[0, 0, 0.75, 1])
-# plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0)
+# plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0, fontsize='large')
 # plt.title('Percentages of Rare Functions Grouped by EC Class and Subclass')
 # # plt.xticks([1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7.5])
+# plot.xaxis.label.set_size(14)
+# plot.yaxis.label.set_size(14)
 # plt.show()
 
-# ##This is the counts of each rare function_______________________________________________________________________________________
+##This is the counts of each rare function_______________________________________________________________________________________
 # activated_sludge['EC Class'] = activated_sludge['EC Number'].apply(EC_parse2)
 # activated_sludge3 = activated_sludge[['EC Class', 'Percentage', 'Name']]
 # print(activated_sludge3)
@@ -43,17 +45,19 @@ activated_sludge2 = activated_sludge[['EC Class and Subclass', 'Percentage', 'Na
 # plt.show()
 
 
-activated_sludge_all = activated_sludge_all[activated_sludge_all['Percentage'] !=0.0]
-print(activated_sludge_all)
+# activated_sludge_all = activated_sludge_all[activated_sludge_all['Percentage'] !=0.0]
+# print(activated_sludge_all)
 
 ##This is visualizing the displaced functions in the entire funcitonal profile________________________________________________________
 plt.figure(figsize=(12, 12))
-sns.scatterplot(data= activated_sludge_all, x='Percentage', y='EC Number', color = 'grey', label = 'All Biome')
-sns.scatterplot(data= activated_sludge, x='Percentage', y='EC Number', color='blue', hue = 'Name', palette='rocket', s=75)
+sns.scatterplot(data= activated_sludge_all, x='Percentage', y='EC Number', color = 'grey', label = 'All Biome', legend=False)
+plot = sns.scatterplot(data= activated_sludge, x='Percentage', y='EC Number', color='blue', hue = 'Name', palette='rocket', s=75, legend=False)
 plt.subplots_adjust(left=0.15, right=0.85, top=0.85, bottom=0.15)  # Adjust margins
 # plt.yticks(rotation=75)
 y_values = activated_sludge['EC Number'].values
 plt.yticks(ticks=[], rotation = 45)
 plt.title('Metagenome Displaced Functions Overlayed on All Biome Functions')
+plot.xaxis.label.set_size(14)
+plot.yaxis.label.set_size(14)
 plt.tick_params(left=False)
 plt.show()
